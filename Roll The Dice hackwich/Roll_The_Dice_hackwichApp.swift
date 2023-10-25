d128ac9b3c7bfcb6be0f442d11ea804f95b8b96c
chris robles <croblesquezad@bsd220.org>
2023-10-25 09:47:28 -0500

    Initial Commit

---
+++ Roll The Dice hackwich/Roll_The_Dice_hackwichApp.swift
@@ -0,0 +1,17 @@
+//
+//  Roll_The_Dice_hackwichApp.swift
+//  Roll The Dice hackwich
+//
+//  Created by christopher robles quezada on 10/25/23.
+//
+
+import SwiftUI
+
+@main
+struct Roll_The_Dice_hackwichApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
